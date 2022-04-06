fe3c2486df0c3bde76e20756fec771f6fcdcb89c
9phanuwat <phanuwat1996@hotmail.com>
2022-04-06 18:38:49 +0700

    [Edited]:

--- src/components/Challenges/Palindromic.js
+++ src/components/Challenges/Palindromic.js
@@ -50,43 +50,43 @@ export default function Palindromic() {
                 <Header as='h1'>Palindromic JavaScript</Header>
                 <Grid divided='vertically' style={{ marginTop: '30px' }}>
                     <Grid.Row columns={2}>
-                    <Grid.Column>
-                        <div>
-                            <Header as='h3'>INPUT</Header>
-                            <Input 
-                                placeholder='type some string' 
-                                fluid
-                                value={palindrome}
-                                onChange={(event, {value}) => setPalindrome(value)}
-                            />
-                        </div>
+                        <Grid.Column mobile={16} tablet={16} computer={8} style={{ marginBottom: '50px' }}>
+                            <div>
+                                <Header as='h3'>INPUT</Header>
+                                <Input 
+                                    placeholder='type some string' 
+                                    fluid
+                                    value={palindrome}
+                                    onChange={(event, {value}) => setPalindrome(value)}
+                                />
+                            </div>
 
-                        <div style={{ marginTop: '20px' }}>
-                            <Button animated='fade' onClick={()=>handleCheckPalindromic()}>
-                                <Button.Content visible>Submit</Button.Content>
-                                <Button.Content hidden>Submit</Button.Content>
-                            </Button>
-                        </div>
-                        <Header as='h3'>OUTPUT</Header>
-                        <Form>
-                            <TextArea 
-                                placeholder='Result' 
-                                rows={2}
-                                value={output}
-                            />
-                        </Form>
-                    </Grid.Column>
-                    <Grid.Column>
-                        <Header as='h3'>CODE SANDBOX</Header>
-                        <Segment inverted>
-                            <iframe
-                                src='https://codesandbox.io/embed/festive-moon-9d95cv?fontsize=14&hidenavigation=1&theme=dark'
-                                width= '100%'
-                                height= '600px'
-                                title='paalidrome'
-                            />
-                        </Segment>
-                    </Grid.Column>
+                            <div style={{ marginTop: '20px' }}>
+                                <Button animated='fade' onClick={()=>handleCheckPalindromic()}>
+                                    <Button.Content visible>Submit</Button.Content>
+                                    <Button.Content hidden>Submit</Button.Content>
+                                </Button>
+                            </div>
+                            <Header as='h3'>OUTPUT</Header>
+                            <Form>
+                                <TextArea 
+                                    placeholder='Result' 
+                                    rows={2}
+                                    value={output}
+                                />
+                            </Form>
+                        </Grid.Column>
+                        <Grid.Column mobile={16} tablet={16} computer={8}>
+                            <Header as='h3'>CODE SANDBOX</Header>
+                            <Segment inverted>
+                                <iframe
+                                    src='https://codesandbox.io/embed/festive-moon-9d95cv?fontsize=14&hidenavigation=1&theme=dark'
+                                    width= '100%'
+                                    height= '600px'
+                                    title='paalidrome'
+                                />
+                            </Segment>
+                        </Grid.Column>
                     </Grid.Row>
                 </Grid>
             </Container>
